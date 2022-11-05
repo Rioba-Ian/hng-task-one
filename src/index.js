fb461fab03e81a34aa27eeadbc4fcba43cbf03f0
Rioba-Ian-Felix <riobafelix@gmail.com>
2022-11-05 13:29:33 +0300

    "fix: hashrouter for gh pages --add base"

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter base="/">
       <App />
     </HashRouter>
   </React.StrictMode>
